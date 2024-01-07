de3cc1a00dea9c56f3a45bdcfd5d86b94d7eae4f
Didar <damangeldiyev@crystalspring.kz>
2024-01-07 18:10:05 +0600

    fix encoding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
 res.end('Тестовое задание выполнено успешно!\n');
 });
 const port = 3000;
